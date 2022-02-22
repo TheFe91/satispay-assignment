c3b4001798c2c8bd940218ed56fe035569e465bc
Alessandro Defendenti <alessandro.defendenti@radicalbit.io>
2022-02-22 12:59:31 +0100

    implemented server

--- packages/server/models/pokemons.ts
+++ packages/server/models/pokemons.ts
@@ -1,7 +1,6 @@
-import { pipe } from "fp-ts/lib/pipeable";
 import * as O from "fp-ts/lib/Option";
 import * as A from "fp-ts/lib/Array";
-import { identity } from "fp-ts/lib/function";
+import { identity, pipe } from "fp-ts/lib/function";
 import { data } from "../data/pokemons";
 import { toConnection, slice } from "../functions";
 import { Connection } from "../types";
@@ -43,8 +42,42 @@ export function query(args: {
     data,
     filterByQ,
     sliceByAfter,
-    // slicing limit + 1 because the `toConnection` function should known the connection size to determine if there are more results
+    // slicing limit + 1 because the `toConnection` function should know the connection size to determine if there are more results
     slice(0, limit + 1)
   );
   return toConnection(results, limit);
+}
+
+export function queryType(args: {
+  type: string;
+  after?: string;
+  limit?: number;
+}): Connection<Pokemon>  {
+  const { type: queryType, after, limit = SIZE } = args;
+
+  const filterByType: (as: Pokemon[]) => Pokemon[] =
+      queryType === undefined
+          ? identity
+          : A.filter(p => p.types.map((t) => t.toLowerCase()).includes(queryType.toLowerCase()));
+
+  const sliceByAfter: (as: Pokemon[]) => Pokemon[] =
+      // filter only if q is defined
+      after === undefined
+          ? identity
+          : as =>
+              pipe(
+                  as,
+                  A.findIndex(a => a.id >= after),
+                  O.map(a => a + 1),
+                  O.fold(() => as, idx => as.slice(idx))
+              );
+
+  const results: Pokemon[] = pipe(
+      data,
+      filterByType,
+      sliceByAfter,
+      slice(0, limit + 1)
+  );
+
+  return toConnection(results, SIZE);
 }
