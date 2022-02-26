401311c64d17ed95a04675517d86a8bbbd596900
Alessandro Defendenti <alessandro.defendenti@radicalbit.io>
2022-02-26 18:49:11 +0100

    improved error with message

--- packages/client/src/components/Error/index.tsx
+++ packages/client/src/components/Error/index.tsx
@@ -1,15 +1,28 @@
 import { Result } from 'antd';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import selectors from '@State/selectors';
+
+const { getDataError } = selectors;
 
 function Error() {
+  const dataError = useSelector(getDataError);
+
   return (
     <Result
-      status="warning"
+      status="error"
       title={(
         <>
           There were issues while retrieving the data
           <br />
           Please reload the page and try again
+
+          <br />
+
+          Error is:
+          {' '}
+
+          {dataError && dataError.message}
         </>
       )}
     />
