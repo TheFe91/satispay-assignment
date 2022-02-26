71185b9ab914531d1e3335f9fdbe5dcf091e9657
Alessandro Defendenti <alessandrodefendenti@gmail.com>
2022-02-26 18:49:50 +0100

    FEATURE: Error and loading management (#5)
    * managed errors and loading states
    
    * improved error with message
    
    Co-authored-by: Alessandro Defendenti <alessandro.defendenti@radicalbit.io>

--- packages/client/src/components/Error/index.tsx
+++ packages/client/src/components/Error/index.tsx
@@ -1,11 +1,30 @@
 import { Result } from 'antd';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import selectors from '@State/selectors';
+
+const { getDataError } = selectors;
 
 function Error() {
+  const dataError = useSelector(getDataError);
+
   return (
     <Result
-      status="warning"
-      title="There were issues while retrieving the data"
+      status="error"
+      title={(
+        <>
+          There were issues while retrieving the data
+          <br />
+          Please reload the page and try again
+
+          <br />
+
+          Error is:
+          {' '}
+
+          {dataError && dataError.message}
+        </>
+      )}
     />
   );
 }
--- packages/client/src/store/state/selectors.ts
+++ packages/client/src/store/state/selectors.ts
@@ -4,14 +4,34 @@ const getCurrentSearch = ({ pokemon: { currentSearch } }: RootState) => currentS
 const getCurrentTypeFilter = ({ pokemon: { currentTypeFilter } }: RootState) => currentTypeFilter;
 const getCurrentPageSize = ({ pokemon: { currentPageSize } }: RootState) => currentPageSize;
 const getCurrentQuery = ({ pokemon: { currentQuery } }: RootState) => currentQuery;
-const getFilters = ({ pokemon: { filters } }: RootState) => filters;
-const getData = ({ pokemon: { data } }: RootState) => data;
+
+const getIsFetchingFilters = ({ pokemon: { filters } }: RootState) => filters.isFetching;
+const getIsFetchedFilters = ({ pokemon: { filters } }: RootState) => filters.isFetched;
+const getIsErrorFilters = ({ pokemon: { filters } }: RootState) => filters.isError;
+const getFiltersError = ({ pokemon: { filters } }: RootState) => filters.error;
+const getFilters = ({ pokemon: { filters } }: RootState) => filters.item;
+
+const getIsFetchingData = ({ pokemon: { data } }: RootState) => data.isFetching;
+const getIsFetchedData = ({ pokemon: { data } }: RootState) => data.isFetched;
+const getIsErrorData = ({ pokemon: { data } }: RootState) => data.isError;
+const getDataError = ({ pokemon: { data } }: RootState) => data.error;
+const getData = ({ pokemon: { data } }: RootState) => data.item;
 
 export default {
   getCurrentSearch,
   getCurrentTypeFilter,
   getCurrentPageSize,
   getCurrentQuery,
+
+  getIsFetchingFilters,
+  getIsFetchedFilters,
+  getIsErrorFilters,
+  getFiltersError,
   getFilters,
+
+  getIsFetchingData,
+  getIsFetchedData,
+  getIsErrorData,
+  getDataError,
   getData,
 };
